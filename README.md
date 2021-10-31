d0215b2ec73690041d4e1ade15d322b15da5c35b
夜观天象 <jtrslar@163.com>
2021-10-31 14:05:29 +0800

    save

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
-# test_second
+# 预设项目
 
-## Project setup
-```
+- 此项目为预设模板
+
+## 相关指令
+
+```bash
+# 安装以来
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
+#运行项目
 npm run serve
-```
-
-### Compiles and minifies for production
-```
+# 打包项目
 npm run build
 ```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Loading.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="loading_main">
+    <div class="loading_list">
+      <div class="loading_item"></div>
+      <div class="loading_item"></div>
+      <div class="loading_item"></div>
+      <div class="loading_item"></div>
+      <div class="loading_item"></div>
+      <div class="loading_item"></div>
+    </div>
+    <div class="center"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {};
+  },
+  mounted () { },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+@loading_item_size: 2.5vw;
+
+.loading_main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+
+  width: 100%;
+  height: 100%;
+
+  .loading_list {
+    position: relative;
+
+    .loading_item {
+      position: absolute;
+      top: -@loading_item_size / 2;
+      left: -@loading_item_size / 2;
+
+      width: @loading_item_size;
+      height: @loading_item_size;
+
+      animation: loading_rotate 6s linear infinite;
+
+      &:nth-of-type(6n-5) {
+        animation-delay: 0s;
+      }
+      &:nth-of-type(6n-4) {
+        animation-delay: 1s;
+      }
+      &:nth-of-type(6n-3) {
+        animation-delay: 2s;
+      }
+      &:nth-of-type(6n-2) {
+        animation-delay: 3s;
+      }
+      &:nth-of-type(6n-1) {
+        animation-delay: 4s;
+      }
+      &:nth-of-type(6n) {
+        animation-delay: 5s;
+      }
+    }
+  }
+
+  .center {
+    position: absolute;
+
+    border: 3px #000 solid;
+    width: 20px;
+    height: 20px;
+
+    transform: rotate(45deg);
+
+    animation: center_run 1s ease infinite;
+  }
+}
+
+@keyframes loading_rotate {
+  0% {
+    border: 3px #000 solid;
+    border-radius: 0%;
+    transform: rotate(0) scale(1);
+  }
+
+  100% {
+    border: 3px rgba(255, 255, 255, 0) solid;
+    border-radius: 10%;
+    transform: rotate(100deg) scale(35);
+  }
+}
+@keyframes center_run {
+  0% {
+    transform: scale(1);
+    border-radius: 0;
+    opacity: 1;
+  }
+  50% {
+    transform: scale(0.1);
+    border-radius: 50%;
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1);
+    border-radius: 0;
+    opacity: 1;
+  }
+}
+</style>
