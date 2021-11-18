89dac7845ec0b590025d9ca502dbc136ca0c50f0
夜观天象 <jtrslar@163.com>
2021-11-19 00:47:26 +0800

    完成第一个接口调用

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,36 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Loading from "../views/Loading.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/Home.vue'
+import Loading from '@/views/Loading.vue'
+import BlogDetail from '@/views/blog/BlogDetail.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Loading",
-    component: Loading,
+    path: '/',
+    name: 'BlogDetail',
+    component: BlogDetail,
   },
   {
-    path: "/home",
-    name: "Home",
+    path: '/home',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/loading",
-    name: "Loading",
+    path: '/loading',
+    name: 'Loading',
     component: Loading,
   },
-];
+  {
+    path: '/blogDetail',
+    name: 'BlogDetail',
+    component: BlogDetail,
+  },
+]
 
 const router = new VueRouter({
   routes,
-});
+})
 
-export default router;
+export default router
---
+++ src/views/blog/BlogDetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- 可能会改成组件 -->
+  <div class="main">
+    <div ref="blogDetail" class="blog_detail"></div>
+  </div>
+</template>
+
+<script>
+import { marked } from 'marked'
+import API from '@/api/index'
+
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    this.getBLogDetail()
+  },
+  methods: {
+    async getBLogDetail() {
+      const parmas = { pageSize: 10, pageNum: 1 }
+      const blogContent = (await API.getMdFile(parmas)) || ''
+
+      const blogDom = this.$refs.blogDetail
+      // const content = marked(blogContent)
+      console.log('content is ===>>> ', blogContent)
+      blogDom.innerHTML = blogContent
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.main {
+  width: 100%;
+  height: 100%;
+
+  font-family: 'SansHansLight';
+  font-size: 26px;
+
+  .blog_detail {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
